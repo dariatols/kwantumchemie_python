fb172c349566a251011c0cc88d9ebe46833e3637
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 14:45:11 +0200

    oef 1

--- info.py
+++ info.py
@@ -1,18 +1,48 @@
-from ipywidgets import widgets
-from IPython.display import display
-import math
 import numpy as np
-import matplotlib
 from matplotlib import pyplot as plt
 
-class Info():
+class wc1_oef1():
     @staticmethod
-    def twc01oef01deel1(b):
+    def t_deel1(b):
         print('Maak een functie die enkel het sinusgedeelte',
               'van de golfvergelijking teruggeeft. Hierbij maak je gebruik',
-              'van numpy.sin() en numpy.pi.')
+              'van numpy.sin(...) en numpy.pi.')
 
     @staticmethod
-    def awc01oef01deel1(b):
+    def a_deel1(b):
         print('De functie geeft np.sin( (n*np.pi*x)/l ) terug.')
+
+    @staticmethod
+    def t_deel2(b):
+        print('Gebruik hiervoor np.arange(...) of np.linspace(...)')
+
+    @staticmethod
+    def a_deel2(b):
+        x = np.linspace(0, 10, 1000)
+        print(x)
+
+    @staticmethod
+    def t_deel3(b):
+        print('Gebruik plt.plot(...) om de drie toestanden te plotten.')
+
+    @staticmethod
+    def a_deel3(b):
+        def tijdsonafh(x, n, l):
+            return np.sin((n * np.pi * x) / l)
+
+        x = np.linspace(0, 10, 1000)
+        toestanden = 3
+
+        fig, ax = plt.subplots()
+        ax.set_xlabel('positie')
+        ax.set_ylabel('amplitude')
+
+        for n in range(1, toestanden + 1):
+            harm_osc = tijdsonafh(x, n, 10)
+
+            ax.plot(x, harm_osc, label='n={}'.format(n))
+
+        ax.axhline(linewidth=.5, color='k')
+        ax.legend()
+        fig.show()
 
