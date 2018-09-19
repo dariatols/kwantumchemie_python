de4c6273ea454c787833cd677f7785fc654c209f
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 13:49:00 +0200

    zonder __init__

--- info.py
+++ info.py
@@ -2,9 +2,6 @@ from ipywidgets import widgets
 from IPython.display import display
 
 class Info():
-    def __init__(self):
-        pass
-
     def tip_wc1_oef1(self, b):
         print('Dit is een tip voor deze vraag.')
 
