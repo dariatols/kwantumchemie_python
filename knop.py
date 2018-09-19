f3595e3ab4e67b22845eccaca7c3aa5a318b7657
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 11:55:13 +0200

    testversie library

---
+++ knop.py
@@ -0,0 +1,22 @@
+from ipywidgets import widgets
+from IPython.display import display
+
+class Knop():
+    def __init__(self, soort):
+        if soort == 'tip':
+            self.knop = widgets.Button(description='Tip')
+        else:
+            self.knop = widgets.Button(description='Antwoord')
+
+        display(self.knop)
+
+
+    def tip_wc1_oef1(self):
+        print('Dit is een tip voor deze vraag.')
+
+    def antw_wc1_oef1(self):
+        print('Dit is het antwoord.')
+
+
+
+knop = Knop('tip')
