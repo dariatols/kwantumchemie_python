5d155847eaf2231987a8d0a6310042b12da019f4
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 15:53:36 +0200

    oefening 3

--- info.py
+++ info.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import simps
+from scipy.integrate import quad
 
 class wc1_oef1():
     @staticmethod
@@ -100,7 +101,7 @@ class wc1_oef2():
             return np.sqrt(simps(pd, x))
 
         psi_x = golffunctie()
-        print(norm(psi_x))
+        print('de norm is: ', norm(psi_x))
 
     @staticmethod
     def t_deel4(b):
@@ -144,3 +145,90 @@ class wc1_oef2():
 
         ax.legend()
         fig.show()
+
+class wc1_oef3():
+
+    @staticmethod
+    def t_deel1(b):
+        print('Tip:')
+        print('Gebruik hiervoor np.exp(...) en np.sqrt(...). We gebruiken',
+              'de NumPy modules omdat we hier met een grid zullen werken.')
+
+    @staticmethod
+    def a_deel1(b):
+        print('Antwoord:')
+        print('In de coordinatenruimte: np.exp(-(x**2/(2.0*sigma**2)))/np.sqrt(sigma*np.sqrt(np.pi))')
+        print('In de momentenruimte: np.exp(-(((p**2)*(sigma**2))/(2.0)))/np.sqrt(np.sqrt(np.pi))')
+
+    @staticmethod
+    def t_deel2(b):
+        print('Tip:')
+        print('Maak een grid van x door gebruik te maken van np.linspace(...).',
+              'Gebruik de vorige functies (coordinatenruimte en momentenruimte)',
+              'met x als input en plot deze grafieken op 1 plot.')
+
+    @staticmethod
+    def a_deel2(b):
+        print('Antwoord:')
+
+        def gauss_x(x):
+            return np.exp(-(x ** 2 / (2.0 * sigma ** 2))) / np.sqrt(sigma * np.sqrt(np.pi))
+
+        def gauss_p(p):
+            return np.sqrt(sigma) * np.exp(-(((p ** 2) * (sigma ** 2)) / (2.0))) / np.sqrt(np.sqrt(np.pi))
+
+        sigma = 0.1
+        grid = np.linspace(-10, 10, 1000)
+
+        fig, ax = plt.subplots()
+
+        ax.plot(grid, gauss_x(grid), label='positie')
+        ax.plot(grid, gauss_p(grid), label='momentum')
+
+        ax.legend()
+        fig.show()
+
+    @staticmethod
+    def t_deel3(b):
+        print('Tip:')
+        print('Gebruik scipy.integrate.quad(...) om te integreren.')
+
+    @staticmethod
+    def a_deel3(b):
+        print('Antwoord:')
+
+        def gauss_x(x):
+            return np.exp(-(x ** 2 / (2.0 * sigma ** 2))) / np.sqrt(sigma * np.sqrt(np.pi))
+
+        def gauss_p(p):
+            return np.sqrt(sigma) * np.exp(-(((p ** 2) * (sigma ** 2)) / (2.0))) / np.sqrt(np.sqrt(np.pi))
+
+        sigma = 0.1
+        grid = np.linspace(-10, 10, 1000)
+
+        def x_int(x):
+            return gauss_x(x) * x * gauss_x(x)
+
+        def x2_int(x):
+            return gauss_x(x) * (x ** 2) * gauss_x(x)
+
+        def p_int(p):
+            return gauss_p(p) * p * gauss_p(p)
+
+        def p2_int(p):
+            return gauss_p(p) * (p ** 2) * gauss_p(p)
+
+        sigma = 0.1
+
+        # positie
+        exp_x = quad(x_int, -np.inf, np.inf)[0]
+        exp_x2 = quad(x2_int, -np.inf, np.inf)[0]
+        delta_x = np.sqrt(exp_x2 - exp_x ** 2)
+
+        # momentum
+        exp_p = quad(p_int, -np.inf, np.inf)[0]
+        exp_p2 = quad(p2_int, -np.inf, np.inf)[0]
+        delta_p = np.sqrt(exp_p2 - exp_p ** 2)
+
+        onzekerheid = delta_x * delta_p
+        print('De onzekerheid bedraagt ', onzekerheid)
