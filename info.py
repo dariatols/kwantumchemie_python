c22514c84c043043dd53fd5048fff232650f2a6e
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 13:05:40 +0200

    weer met klassen

---
+++ info.py
@@ -0,0 +1,12 @@
+from ipywidgets import widgets
+from IPython.display import display
+
+class Info():
+    def __init__(self):
+        pass
+
+    def tip_wc1_oef1(self):
+        print('Dit is een tip voor deze vraag.')
+
+    def antw_wc1_oef1(self):
+        print('Dit is het antwoord.')
--- knop.py
+++
@@ -1,9 +0,0 @@
-from ipywidgets import widgets
-from IPython.display import display
-
-
-def tip_wc1_oef1():
-    print('Dit is een tip voor deze vraag.')
-
-def antw_wc1_oef1():
-    print('Dit is het antwoord.')
