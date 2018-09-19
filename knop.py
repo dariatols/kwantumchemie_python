f93a3e8acf1fec1402609d370d646e797ebb0700
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 13:03:23 +0200

    geen klassen meer

--- knop.py
+++ knop.py
@@ -1,18 +1,9 @@
 from ipywidgets import widgets
 from IPython.display import display
 
-class Knop():
-    def __init__(self, soort):
-        if soort == 'tip':
-            self.knop = widgets.Button(description='Tip')
-        else:
-            self.knop = widgets.Button(description='Antwoord')
 
-        return self.knop
+def tip_wc1_oef1():
+    print('Dit is een tip voor deze vraag.')
 
-
-    def tip_wc1_oef1(self):
-        print('Dit is een tip voor deze vraag.')
-
-    def antw_wc1_oef1(self):
-        print('Dit is het antwoord.')
+def antw_wc1_oef1():
+    print('Dit is het antwoord.')
