43132e61a2c2a0ab8c289db3cc0c1e6f0029e2ab
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 14:28:30 +0200

    static method aangepast

--- info.py
+++ info.py
@@ -7,14 +7,12 @@ from matplotlib import pyplot as plt
 
 class Info():
     @staticmethod
-    def twc01oef01deel1(self, b):
-        print('Maak een functie die enkel het sinusgedeelte' \\
-              'van de golfvergelijking teruggeeft. Hierbij maak je gebruik' \\
+    def twc01oef01deel1():
+        print('Maak een functie die enkel het sinusgedeelte',
+              'van de golfvergelijking teruggeeft. Hierbij maak je gebruik',
               'van numpy.sin() en numpy.pi.')
 
     @staticmethod
-    def awc01oef01deel1(self, b):
+    def awc01oef01deel1(b):
         print('De functie geeft np.sin( (n*np.pi*x)/l ) terug.')
 
-
-#print('Hello world!')
