db403f4e4f189a9ddc21891c8068b4ebbbb7f9d0
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 13:58:51 +0200

    static method

--- info.py
+++ info.py
@@ -2,8 +2,10 @@ from ipywidgets import widgets
 from IPython.display import display
 
 class Info():
+    @staticmethod
     def tip_wc1_oef1(self, b):
         print('Dit is een tip voor deze vraag.')
 
+    @staticmethod
     def antw_wc1_oef1(self, b):
         print('Dit is het antwoord.')
