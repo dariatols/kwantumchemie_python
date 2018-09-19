28ef43353a5bacedf4e8ceb8820fb3c08403304e
dtolstyk <Daria.Tolstykh@UGent.be>
2018-09-19 14:23:45 +0200

    oef1 deelvraag 1 test

--- info.py
+++ info.py
@@ -1,11 +1,20 @@
 from ipywidgets import widgets
 from IPython.display import display
+import math
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 
 class Info():
     @staticmethod
-    def tip_wc1_oef1(self, b):
-        print('Dit is een tip voor deze vraag.')
+    def twc01oef01deel1(self, b):
+        print('Maak een functie die enkel het sinusgedeelte' \\
+              'van de golfvergelijking teruggeeft. Hierbij maak je gebruik' \\
+              'van numpy.sin() en numpy.pi.')
 
     @staticmethod
-    def antw_wc1_oef1(self, b):
-        print('Dit is het antwoord.')
+    def awc01oef01deel1(self, b):
+        print('De functie geeft np.sin( (n*np.pi*x)/l ) terug.')
+
+
+#print('Hello world!')
